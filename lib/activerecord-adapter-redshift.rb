6e322b23275aab63266705f2e1150a2ac3b63d84
David Arendsen GP <155541190+darendsen@users.noreply.github.com>
2024-08-28 16:12:53 +0200

    Delete lib/activerecord-adapter-redshift.rb

--- lib/activerecord-adapter-redshift.rb
+++
@@ -1,5 +0,0 @@
-if ActiveRecord.version >= Gem::Version.new('7.2.0')
-  require "active_record"
-  require "active_record/connection_adapters"
-  ActiveRecord::ConnectionAdapters.register("redshift", "ActiveRecord::ConnectionAdapters::RedshiftAdapter", "active_record/connection_adapters/redshift_adapter")
-end
