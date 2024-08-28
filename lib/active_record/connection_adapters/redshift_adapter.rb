f99acefb5aa1d3fc96cf59f77d2db3589dce08d8
David Arendsen GP <155541190+darendsen@users.noreply.github.com>
2024-08-28 16:11:22 +0200

    Register the adapter for ActiveRecord 7.2 and higher

--- lib/active_record/connection_adapters/redshift_adapter.rb
+++ lib/active_record/connection_adapters/redshift_adapter.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'pg'
+require "active_record"
+require "active_record/connection_adapters"
 
 if ActiveRecord.version >= Gem::Version.new('7.1.0')
   require_relative 'redshift_7_1_adapter'
@@ -8,4 +10,8 @@ elsif ActiveRecord.version >= Gem::Version.new('7.0.0')
   require_relative 'redshift_7_0_adapter'
 else
   raise 'no compatible version of ActiveRecord detected'
+end
+
+if ActiveRecord.version >= Gem::Version.new('7.2.0')
+  ActiveRecord::ConnectionAdapters.register("redshift", "ActiveRecord::ConnectionAdapters::RedshiftAdapter", "active_record/connection_adapters/redshift_adapter")
 end
