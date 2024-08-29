105c43356e4ed5ba7c6976a5edb860917b9d4c7f
Yoshiteru Negishi <yoshiteru-negishi@locoguide.co.jp>
2024-08-29 12:13:18 +0900

    Stop calling not implemented super method

--- lib/active_record/connection_adapters/redshift_7_2/quoting.rb
+++ lib/active_record/connection_adapters/redshift_7_2/quoting.rb
@@ -24,7 +24,7 @@ module ActiveRecord
 
           # Quotes column names for use in SQL queries.
           def quote_column_name(name) # :nodoc:
-            QUOTED_COLUMN_NAMES[name] ||= PG::Connection.quote_ident(super).freeze
+            QUOTED_COLUMN_NAMES[name] ||= PG::Connection.quote_ident(name.to_s).freeze
           end
 
           def column_name_matcher
